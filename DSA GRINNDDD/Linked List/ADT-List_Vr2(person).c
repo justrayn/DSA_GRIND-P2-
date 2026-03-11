836553df81c0cad63e08a0478953c4200bf81383
itmayraynforawhile <161039658+justrayn@users.noreply.github.com>
2026-03-11 10:10:28 +0800

    Refactor main function for better structure

--- DSA GRINNDDD/Linked List/ADT-List_Vr2(person).c
+++ DSA GRINNDDD/Linked List/ADT-List_Vr2(person).c
@@ -30,86 +30,131 @@ Person retrieve(List *list, int index);
 int locate(List *list, Person data);
 void display(List *list);
 
-int main() {
+int main (){
+
     List *list = initialize();
-    int op, cloc, found;
-    Person tempP;
 
-    while (1) {
-        printf("\n\n--- Person List Manager (Linked) ---");
-        printf("\n1. Insert First  2. Insert Last  3. Insert Pos");
-        printf("\n4. Delete Start  5. Delete Last   6. Delete Pos");
-        printf("\n7. Locate Person 8. Display List  0. Exit Program");
+    while(1){
+        int op, cloc, found;
+        Person tempP;
+
+        printf("\n\n--Please choose an operation--\n");
+        printf("\n0.exit program");
+        printf("\n1.insertFirst");
+        printf("\n2.insertLast");
+        printf("\n3.insertPos");
+        printf("\n4.deleteStart");
+        printf("\n5.deleteLast");
+        printf("\n6.deletePos");
+        printf("\n7.locate");
+        printf("\n8.display all");
         printf("\nInput operation: ");
-        
-        if (scanf("%d", &op) != 1) break;
+        scanf("%d", &op);
 
-        switch (op) {
+        switch(op){
+
             case 0:
                 empty(list);
                 free(list);
-                printf("Memory cleared. Goodbye!\n");
+                printf("\nMemory cleared. Goodbye!\n");
                 exit(0);
+                break;
 
             case 1:
-            case 2:
-            case 3:
+                printf("\nUsing insertFirst...\n");
                 printf("Enter Name: ");
                 scanf("%s", tempP.name);
                 printf("Enter Age: ");
                 scanf("%d", &tempP.age);
-                
-                if (op == 1) insertFirst(list, tempP);
-                else if (op == 2) insertLast(list, tempP);
-                else {
-                    printf("Enter position (0-%d): ", list->count);
-                    scanf("%d", &cloc);
-                    insertPos(list, tempP, cloc);
-                }
+
+                insertFirst(list, tempP);
+                break;
+
+            case 2:
+                printf("\nUsing insertLast...\n");
+                printf("Enter Name: ");
+                scanf("%s", tempP.name);
+                printf("Enter Age: ");
+                scanf("%d", &tempP.age);
+
+                insertLast(list, tempP);
+                break;
+
+            case 3:
+                printf("\nUsing insertPos...\n");
+                printf("Enter Name: ");
+                scanf("%s", tempP.name);
+                printf("Enter Age: ");
+                scanf("%d", &tempP.age);
+
+                printf("Enter position (0-%d): ", list->count);
+                scanf("%d", &cloc);
+
+                insertPos(list, tempP, cloc);
                 break;
 
             case 4:
-                if (list->count > 0) deleteStart(list);
-                else printf("List is empty.\n");
+                if(list->count == 0){
+                    printf("\nNothing to delete...\n");
+                } else {
+                    printf("\nUsing deleteStart...\n");
+                    deleteStart(list);
+                }
                 break;
 
             case 5:
-                if (list->count > 0) deleteLast(list);
-                else printf("List is empty.\n");
+                if(list->count == 0){
+                    printf("\nNothing to delete...\n");
+                } else {
+                    printf("\nUsing deleteLast...\n");
+                    deleteLast(list);
+                }
                 break;
 
             case 6:
-                if (list->count == 0) {
-                    printf("List is empty.\n");
+                if(list->count == 0){
+                    printf("\nNothing to delete...\n");
                 } else {
-                    printf("Enter index to delete (0-%d): ", list->count - 1);
+                    printf("\nUsing deletePos...\n");
+                    printf("Enter index to delete: ");
                     scanf("%d", &cloc);
+
                     deletePos(list, cloc);
                 }
                 break;
 
             case 7:
-                printf("Enter exact Name to find: ");
-                scanf("%s", tempP.name);
-                printf("Enter Age: ");
-                scanf("%d", &tempP.age);
-                found = locate(list, tempP);
-                if (found != -1) printf("Found at index: %d\n", found);
-                else printf("Person not found.\n");
+                if(list->count == 0){
+                    printf("\nNothing to locate...\n");
+                } else {
+                    printf("\nUsing locate...\n");
+                    printf("Enter Name: ");
+                    scanf("%s", tempP.name);
+                    printf("Enter Age: ");
+                    scanf("%d", &tempP.age);
+
+                    found = locate(list, tempP);
+
+                    if(found == -1){
+                        printf("\nPerson not found.\n");
+                    } else {
+                        printf("\n%s found at index %d\n", tempP.name, found);
+                    }
+                }
                 break;
 
             case 8:
+                printf("\nDisplaying list...\n");
                 display(list);
                 break;
 
             default:
-                printf("Invalid option.\n");
+                printf("\nInvalid operation.\n");
         }
     }
 
     return 0;
 }
-
 List* initialize(){
     List *newList = malloc(sizeof(List));
     if(newList == NULL){
